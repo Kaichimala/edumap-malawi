8576da57abaa1c69f41b3cc68cc021305b0259c9
Austin Mubaya <bsc-inf-09-21@unima.ac.mw>
2026-04-09 14:09:25 +0200

    Update Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
     }
   }
 
-  /* ─── Sign Up ────────────────────────────────────────────────────────────── */
+  /*Signup */
   async function handleSignUp(e) {
     e.preventDefault()
     reset()
